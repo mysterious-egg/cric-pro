3d1599ea059eb2958a28123c68f21d6794db43f0
mysterious-egg <71733395+mysterious-egg@users.noreply.github.com>
2024-01-16 04:44:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,163 @@
+
+
+var sehwagImage = document.querySelector('.sehwag');
+var sehwagName = document.querySelector('.SehwagName');
+
+sehwagImage.style.visibility = "hidden";
+
+sehwagName.addEventListener("click", function() {
+    if (sehwagImage.style.visibility == "hidden") {
+        sehwagImage.style.visibility = "visible";
+    } else {
+        sehwagImage.style.visibility = "hidden";
+    }
+});
+
+
+
+var sachinImage = document.querySelector('.sachin');
+var sachinName = document.querySelector('.SachinName');
+
+sachinImage.style.visibility = "hidden";
+
+sachinName.addEventListener("click", function() {
+    if (sachinImage.style.visibility == "hidden") {
+        sachinImage.style.visibility = "visible";
+    } else {
+        sachinImage.style.visibility = "hidden";
+    }
+});
+
+
+
+var gambhirImage = document.querySelector('.gambhir');
+var gambhirName = document.querySelector('.GambhirName');
+
+gambhirImage.style.visibility = "hidden";
+
+gambhirName.addEventListener("click", function() {
+    if (gambhirImage.style.visibility == "hidden") {
+        gambhirImage.style.visibility = "visible";
+    } else {
+        gambhirImage.style.visibility = "hidden";
+    }
+});
+
+
+
+var viratImage = document.querySelector('.virat');
+var viratName = document.querySelector('.ViratName');
+
+viratImage.style.visibility = "hidden";
+
+viratName.addEventListener("click", function() {
+    if (viratImage.style.visibility == "hidden") {
+        viratImage.style.visibility = "visible";
+    } else {
+        viratImage.style.visibility = "hidden";
+        console.log(sehwagImage.style)
+    }
+});
+
+
+var dhoniImage = document.querySelector('.dhoni');
+var dhoniName = document.querySelector('.DhoniName');
+
+dhoniImage.style.visibility = "hidden";
+
+dhoniName.addEventListener("click", function() {
+    if (dhoniImage.style.visibility == "hidden") {
+        dhoniImage.style.visibility = "visible";
+    } else {
+        dhoniImage.style.visibility = "hidden";
+    }
+});
+
+
+var yuvrajImage = document.querySelector('.yuvraj');
+var yuvrajName = document.querySelector('.YuvrajName');
+
+yuvrajImage.style.visibility = "hidden";
+
+yuvrajName.addEventListener("click", function() {
+    if (yuvrajImage.style.visibility == "hidden") {
+        yuvrajImage.style.visibility = "visible";
+    } else {
+        yuvrajImage.style.visibility = "hidden";
+    }
+});
+
+
+var sureshImage = document.querySelector('.suresh');
+var sureshName = document.querySelector('.SureshName');
+
+sureshImage.style.visibility = "hidden";
+
+sureshName.addEventListener("click", function() {
+    if (sureshImage.style.visibility == "hidden") {
+        sureshImage.style.visibility = "visible";
+    } else {
+        sureshImage.style.visibility = "hidden";
+    }
+});
+
+
+
+var harbhajanImage = document.querySelector('.harbhajan');
+var harbhajanName = document.querySelector('.HarbhajanName');
+
+harbhajanImage.style.visibility = "hidden";
+
+harbhajanName.addEventListener("click", function() {
+    if (harbhajanImage.style.visibility == "hidden") {
+        harbhajanImage.style.visibility = "visible";
+    } else {
+        harbhajanImage.style.visibility = "hidden";
+    }
+});
+
+
+
+var zaheerImage = document.querySelector('.zaheer');
+var zaheerName = document.querySelector('.ZaheerName');
+
+zaheerImage.style.visibility = "hidden";
+
+zaheerName.addEventListener("click", function() {
+    if (zaheerImage.style.visibility == "hidden") {
+        zaheerImage.style.visibility = "visible";
+    } else {
+        zaheerImage.style.visibility = "hidden";
+    }
+});
+
+
+
+
+var munafImage = document.querySelector('.munaf');
+var munafName = document.querySelector('.MunafName');
+
+munafImage.style.visibility = "hidden";
+
+munafName.addEventListener("click", function() {
+    if (munafImage.style.visibility == "hidden") {
+        munafImage.style.visibility = "visible";
+    } else {
+        munafImage.style.visibility = "hidden";
+    }
+});
+
+
+
+var sreesanthImage = document.querySelector('.sreesanth');
+var sreesanthName = document.querySelector('.SreesanthName');
+
+sreesanthImage.style.visibility = "hidden";
+
+sreesanthName.addEventListener("click", function() {
+    if (sreesanthImage.style.visibility == "hidden") {
+        sreesanthImage.style.visibility = "visible";
+    } else {
+        sreesanthImage.style.visibility = "hidden";
+    }
+});
